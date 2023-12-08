73fbbe9427dd8478341c97a5e3c43f8a8d47fcd8
sugar <1007272552@qq.com>
2023-12-09 00:26:41 +0800

    update Drawer

--- src/searchIndex/index.jsx
+++ src/searchIndex/index.jsx
@@ -1,10 +1,9 @@
-import { Input } from 'antd';
+import { Input, Drawer } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import './index.less'
 import { useState } from 'react';
-import Aside from '../aside'
 function App() {
-	let [isshow,setIsshow]=useState(false);
+	let [isshow, setIsshow] = useState(false);
 	function switchContentShow() {
 		setIsshow(!isshow);
 	}
@@ -19,18 +18,32 @@ function App() {
 						搜索界面
 					</div>
 					<div className='searchbar'>
-						<Input style={{boxShadow:"none"}} prefix="hhh" bordered={false} />
+						<Input style={{ boxShadow: "none" }} prefix="hhh" bordered={false} />
 					</div>
 				</div>
-				<Aside isshow={isshow} switchContentShow={switchContentShow}>
-					{
-						Array.from({length:11}).map((v,i)=>(
-							<div key={i} className='testlist'>
-								testlist1111<br/><br/><br/><br/><br/><br/><br/><br/>111111111111111111111111
-							</div>
-						)
-					)}
-				</Aside>
+				<Drawer
+					open={isshow}
+					onClose={switchContentShow}
+					contentWrapperStyle={{
+						"--borderRadius":"15px",
+						height:"300px",
+						margin:"auto 10px auto auto",
+						borderRadius:"var(--borderRadius)",
+					}}
+					styles={{
+						content:{
+							borderRadius: "var(--borderRadius)",
+						},
+						body:{
+							padding:"15px"
+						}
+					}}
+					closeIcon={null}
+				>
+					<p>Some contents.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa..</p>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+				</Drawer>
 			</div>
 		</>
 	)
